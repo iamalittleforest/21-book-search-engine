40d1cec4cac7120d21647cd015f89009d1c6fc87
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-05 23:00:05 -0700

    import AuthenticationError and signToken, define login resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
@@ -11,8 +13,20 @@ const resolvers = {
     createUser: async () => {
 
     },
-    login: async () => {
+    login: async (parent, { email, password }) => {
+      
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('No user associated with this email address');
+      }
 
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password');
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
     saveBook: async () => {
 
