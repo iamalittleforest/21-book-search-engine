cc7b5b07e6ea4f5e8f2abdf788a38582d2dd2272
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-07 22:59:02 -0700

    define login mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,17 @@
 // import dependency
 import { gql } from '@apollo/client';
 
+// define mutaion for login
 export const LOGIN_USER = gql`
-
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 export const ADD_USER = gql`
