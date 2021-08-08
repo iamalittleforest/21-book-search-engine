3aa71eaff8756f3247b17b20d5ebd423322e2f56
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-08 13:20:05 -0700

    add import for useMutation, fix filepath for SAVE_BOOK mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,7 +1,8 @@
 // import dependencies
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-import { SAVE_BOOK } from '../../utils/mutations';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import Auth from '../utils/auth';
