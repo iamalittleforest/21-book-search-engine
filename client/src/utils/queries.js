b1e7e2baa093f63bed0b0bf293811a4ef36c4e44
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-07 22:46:21 -0700

    define me query

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+// import dependency
+import { gql } from '@apollo/client';
+
+// define query
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
