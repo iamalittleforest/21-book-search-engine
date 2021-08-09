6866c7805c6b24e600a9f9b956b31dad60832301
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-08 20:29:08 -0700

    rename input to bookToSave to match variable within SearchBooks.js file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,7 +10,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: BookInput): User
+    saveBook(bookToSave: BookInput): User
     removeBook(bookId: ID!): User
   }
 
